f6a7e8642ad26fca23acd59a47f8913ed5d0882f
Hristo Papucharov <hpapucharov@gmail.com>
2021-05-05 13:44:10 -0500

    Polished Reviews

--- Wayfarer-Angular/src/app/cities/paris/paris.component.ts
+++ Wayfarer-Angular/src/app/cities/paris/paris.component.ts
@@ -7,12 +7,12 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class ParisComponent implements OnInit {
   public commentsArray: any = [
-    {id: 1, avatar: 'assets/images/avatar.jpg', name: 'Dumbledore', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et\n' +
+    {id: 1, avatar: 'assets/images/dumbledore.jpg', name: 'Dumbledore', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et\n' +
         '                     dolore magna aliqua. Dolor magna eget est lorem ipsum dolor sit amet. Malesuada bibendum arcu vitae elementum.'},
-    {id: 2, avatar: 'assets/images/avatar.jpg', name: 'Ron', text: 'Malesuada bibendum arcu vitae elementum.\n' +
+    {id: 2, avatar: 'assets/images/ron.jpg', name: 'Ron', text: 'Malesuada bibendum arcu vitae elementum.\n' +
         '          Lorem ipsum dolor sit amet. Lacus luctus accumsan tortor posuere ac ut.'},
-    {id: 3, avatar: 'assets/images/avatar.jpg', name: 'Harry', text: 'Lorem ipsum dolor sit amet. Lacus luctus accumsan tortor posuere ac ut.'},
-    {id: 4, avatar: 'assets/images/avatar.jpg', name: 'Hermione', text: 'Cursus eget nunc scelerisque viverra mauris. Tellus orci ac auctor augue.\n' +
+    {id: 3, avatar: 'assets/images/harry.jpg', name: 'Harry', text: 'Lorem ipsum dolor sit amet. Lacus luctus accumsan tortor posuere ac ut.'},
+    {id: 4, avatar: 'assets/images/hermione.jpg', name: 'Hermione', text: 'Cursus eget nunc scelerisque viverra mauris. Tellus orci ac auctor augue.\n' +
         '          Et malesuada fames ac turpis egestas sed tempus urna et.'},
   ];
   account = {
